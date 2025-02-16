7f93595ba22592dc89d13f051388ccaada505d52
mcghieb <mcghie.blake@gmail.com>
2025-02-15 20:56:23 -0700

    update(README) coverage badge fix?

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-if [ "$1" == "prd" ]; then
+if [ "$1" = "prd" ]; then
     host=https://pizza-service.byucsstudent.click
 else 
     host=http://localhost:3000
@@ -13,7 +13,7 @@ echo $response
 
 # set up two users
 curl -X POST $host/api/auth -d '{"name":"pizza diner", "email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json'
-curl -X POST $host/api/auth -d '{"name":"pizza franchisee", "email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"pizza pocket owner", "email":"pp@jwt.com", "password":"pizza pocket"}' -H 'Content-Type: application/json'
 
 # set up default menu items
 curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Veggie", "description": "A garden of delight", "image":"pizza1.png", "price": 0.0038 }'  -H "Authorization: Bearer $token"
@@ -23,7 +23,9 @@ curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "titl
 curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Charred Leopard", "description": "For those with a darker side", "image":"pizza5.png", "price": 0.0099 }'  -H "Authorization: Bearer $token"
 
 # add franchise and store
-curl -X POST $host/api/franchise -H 'Content-Type: application/json' -d '{"name": "pizzaPocket", "admins": [{"email": "f@jwt.com"}]}'  -H "Authorization: Bearer $token"
+curl -X POST $host/api/franchise -H 'Content-Type: application/json' -d '{"name": "Pizza Pocket", "admins": [{"email": "pp@jwt.com"}]}'  -H "Authorization: Bearer $token"
 curl -X POST $host/api/franchise/1/store -H 'Content-Type: application/json' -d '{"franchiseId": 1, "name":"SLC"}'  -H "Authorization: Bearer $token"
+curl -X POST $host/api/franchise/1/store -H 'Content-Type: application/json' -d '{"franchiseId": 1, "name":"Orem"}'  -H "Authorization: Bearer $token"
+
 
 echo $token 
